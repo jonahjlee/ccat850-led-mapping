32d48f53926eef45695f00ef49aa1e230dee9491
Jonah Lee <jonahjacqueslee@gmail.com>
2025-02-21 15:42:27 -0800

    add spacing constants

--- led_mapper_850/lay_out_pcb.py
+++ led_mapper_850/lay_out_pcb.py
@@ -7,12 +7,20 @@ class LedPlacer():
     def __init__(self, pcb: pcbnew.BOARD=active_pcb):
         self.pcb = pcb
 
-        # constants: based on led_mapper_850 schematic
+        # constants based on led_mapper_850.kicad_sch
         # rows and columns refer to position on pcb, not in schematic,
         # though the schematic is on the same grid except discontinuities where diagonals are missing
         # for example, D69 is in column 23 and not column 24
         self.num_rows = 12  # actual num rows is 11.5; last row contains only 23 LEDs
         self.num_cols = 46
+
+        # constants based on pixel_loc_analysis.xlsx
+        # LEDs lie on a hexagonal grid composed of equilateral triangles with side length equal to self.spacing_um
+        # this script is intended to generate the top left rhombus -- the other 2 can be obtained through copy/paste/transformations/edits
+        self.spacing_um = 1400.0
+        self.row_shift_um = -self.spacing_um / 2  # rows shift left as we go down
+        self.col_width_um = self.spacing_um
+        self.row_height_um = (self.spacing_um ** 2 - self.row_shift_um ** 2) ** 0.5  # equilateral triangle height
 
     def led_ref(self, row: int, col: int) -> str:
         '''Get the refdes of an LED given its position in the grid.
