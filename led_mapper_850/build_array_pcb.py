508ef1385532cc6602a0180c8eb0e7021d8bf677
Jonah Lee <jonahjacqueslee@gmail.com>
2025-02-26 17:00:08 -0800

    refactor: create main placing script

---
+++ led_mapper_850/build_array_pcb.py
@@ -0,0 +1,31 @@
+from led_placer import LedPlacer
+
+# FRONT LAYER
+
+# Place LEDs
+# Place & route pad 2 vias
+# Route front rows
+# Short front pins
+
+# BACK LAYER
+
+# Route back columns
+# Stitch diagonals
+
+# INNER LAYERS
+
+# Inner layer 1 - stitch rectangles
+# Inner layer 2 - stitch rectangles
+
+# CONNECTOR
+
+# Add & route connector vias
+# Route pins on front layer
+# Route pins on inner layer 1
+
+# MISC
+
+# Route bottom row
+# Left side - route to existing connector via
+# Right side - add new via and route on inner layer 2
+# REPEAT FOR ALL 4 ARRAYS
