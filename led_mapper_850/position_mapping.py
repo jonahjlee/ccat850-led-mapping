3d1f3fab716c5aab53a28fb449e51f34a9c3b30a
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-04 16:39:43 -0800

    add position mapping code

--- led_mapper_850/position_mapping.py
+++ led_mapper_850/position_mapping.py
@@ -5,28 +5,15 @@
 # CCAT Prime 2025
 #
 # Functions to map between LED 'refdes' references, pin logic states
-# and LED positions (row/column, or positions in um.)
+# and LED positions (row/column, or positions in um).
+# NOTE: Pin indices start at 0 -- i.e. a 24 pin connector has pins 0-23, inclusive.
+#       This fact may conflict with KiCAD PCB/Schematic designs
 # ============================================================================ #
 
-import enum
+from connector_state import ConnectorState, TriState
 
-class TriState(enum.Enum):
-    """Representation of a tri-state logic pin.
-    
-    See https://en.wikipedia.org/wiki/Three-state_logic for more
-    """
-    HIGH = 1
-    LOW = 0
-    HI_Z = -1
+def get_() -> None:
+    raise NotImplementedError  # TODO
 
-
-class ConnectorState:
-    """Object representing a tri-state logic connector state."""
-    def __init__(self, pin_state_list: list[TriState]=None, pin_count: int=24):
-
-        if pin_state_list is None:
-            pin_state_list = [TriState.HI_Z for pin in range(pin_count)]
-        
-        assert len(pin_state_list) == pin_count, "pin_state_list must have length equal to pin_count!"
-        self.pin_state_list = pin_state_list
-
+if __name__ == "__main__":
+    cs = ConnectorState(hi=[2, 3, -1], lo=4)
